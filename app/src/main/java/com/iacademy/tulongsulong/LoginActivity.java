c65514dd770abaa7e8fc802830bf4d9c46fc019a
aLai502 <104610544+aLai502@users.noreply.github.com>
2022-11-23 21:22:46 +0800

    Authentication
    Login, Register n Email verification

--- app/src/main/java/com/iacademy/tulongsulong/LoginActivity.java
+++ app/src/main/java/com/iacademy/tulongsulong/LoginActivity.java
@@ -1,14 +1,83 @@
 package com.iacademy.tulongsulong;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView tvRegister;
+    EditText etLogEmail, etLogPassword;
+    Button btnLogin;
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etLogEmail = findViewById(R.id.etLogEmail);
+        etLogPassword = findViewById(R.id.etLogPassword);
+        tvRegister = findViewById(R.id.tvSignup);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+
+    private void loginUser(){
+        String email = etLogEmail.getText().toString();
+        String password = etLogPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email)){
+            etLogEmail.setError("Email cannot be empty");
+            etLogEmail.requestFocus();
+        }else if(TextUtils.isEmpty(password)){
+            etLogPassword.setError("Password cannot be empty");
+            etLogPassword.requestFocus();
+        }else{
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener
+                    (new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(LoginActivity.this, "User logged in successfully",
+                                        Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                finish();
+                            }else{
+                                Toast.makeText(LoginActivity.this, "Login Error: " + task
+                                        .getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
     }
 }
--- app/src/main/java/com/iacademy/tulongsulong/MainActivity.java
+++ app/src/main/java/com/iacademy/tulongsulong/MainActivity.java
@@ -2,13 +2,44 @@ package com.iacademy.tulongsulong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogout;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogout = findViewById(R.id.btnLogout);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null){
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        }
     }
 }
